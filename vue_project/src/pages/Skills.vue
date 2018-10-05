f9172d97b343bee6e843db6f82582e76767d9ffb
Naoki Kishi <naokikishi.kyoto@gmail.com>
2018-10-05 22:34:53 +0900

    shareボタンを追加

--- vue_project/src/pages/Skills.vue
+++ vue_project/src/pages/Skills.vue
@@ -29,6 +29,7 @@ export default {
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
+
 .skills-wrapper {
   text-align: center;
 }
--- vue_project/src/pages/Top.vue
+++ vue_project/src/pages/Top.vue
@@ -1,6 +1,19 @@
 <template>
-    <div class="hello">
+    <div class='hello'>
       <h1>{{ msg }}</h1>
+
+      <h3>Share</h3>
+
+      <div class = 'share-buttons'>
+        <a href='https://twitter.com' target='_blank'>
+          <div class='button fa fa-twitter'>
+          </div>
+        </a>
+        <a href='https://facebook.com' target='_blank'>
+          <div class='button fa fa-facebook'>
+          </div>
+        </a>
+      </div>
     </div>
 </template>
 
@@ -15,45 +28,83 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style lang='scss' scoped>
+@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
 
-.hello{
+.hello {
   height: 94vh;
-  background:linear-gradient(to left, rgb(148, 115, 221) 0%, rgb(79, 210, 219) 100%);
+  background: linear-gradient(
+    to left,
+    rgb(148, 115, 221) 0%,
+    rgb(79, 210, 219) 100%
+  );
   text-align: center;
 }
 
-h1{
-  padding-top:40vh;
-  font-size : 50px;
-  color:white;
+h1 {
+  padding-top: 40vh;
+  font-size: 50px;
+  color: white;
   letter-spacing: 3px;
   font-weight: normal;
-  border-bottom: 1px solid rgba(255,255,255,1);
-  margin:0;
-  text-shadow: 0px 0px 30px rgba(0,0,0,0.2);
+  border-bottom: 1px solid rgba(255, 255, 255, 1);
+  margin: 0;
+  text-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
+}
+
+h3 {
+  color: white;
+  font-weight: normal;
+  font-size: 25px;
+  margin-top: 200px;
+}
+
+.button {
+  width: 100px;
+  height: 100px;
+  background-color: white;
+  border-radius: 100px;
+  display: inline-block;
+  margin: 20px;
+  margin-top:0px;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
+  // cursor: pointer;
+}
+
+.button:hover {
+  background-color: darken(white, 10%);
+}
+
+.fa {
+  color: rgb(148, 115, 221);
+  font-size: 40px;
+  line-height: 100px;
 }
 
 /* タブレット */
 @media (max-width: 1000px) {
-  .hello{
+  .hello {
     height: 88vh;
   }
-  h1{
-    padding-top:30vh;
-    font-size : 40px;
+  h1 {
+    padding-top: 30vh;
+    font-size: 40px;
   }
 }
 /* スマートフォン */
 @media (max-width: 670px) {
-  .hello{
+  .hello {
     height: 76vh;
   }
-  h1{
-    padding-top:20vh;
-    font-size : 30px;
+  h1 {
+    padding-top: 20vh;
+    font-size: 30px;
+  }
+
+  .button{
+    width:30%;
+    // height:200px;
   }
 }
-
 </style>
